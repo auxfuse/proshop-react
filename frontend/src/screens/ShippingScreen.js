abc489c5d652eeca6d9ee28d222a5b818cf74f82
auxfuse <aux.fuse@yahoo.com>
2020-10-26 23:21:25 +0000

    Added Navigational Breadcrump component and injected into ShippingScreen.js

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/formContainer";
 import { saveShippingAddress } from "../actions/cartActions";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingScreen = ({ history }) => {
     const cart = useSelector(state => state.cart)
@@ -28,6 +29,7 @@ const ShippingScreen = ({ history }) => {
 
     return (
         <FormContainer>
+            <CheckoutSteps step1 step2></CheckoutSteps>
             <h1>Shipping</h1>
             <Form onSubmit={submitHandler}>
                 <Form.Group controlId='address'>
