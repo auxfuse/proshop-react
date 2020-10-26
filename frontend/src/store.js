8189d44567d4bfd54b10daeaf138f6e98620ae8f
auxfuse <aux.fuse@yahoo.com>
2020-10-26 23:11:49 +0000

    Shipping Screen and Save Address for Shipping functionality created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -21,9 +21,15 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse(
 const userInfoFromStorage = localStorage.getItem('userInfo') ? JSON.parse(
     localStorage.getItem('userInfo')) : null;
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? JSON.parse(
+    localStorage.getItem('shippingAddress')) : {};
+
 const initialState = {
-    cart: { cartItems: cartItemsFromStorage },
-    userLogin: { userItems: userInfoFromStorage }
+    cart: {
+        cartItems: cartItemsFromStorage,
+        shippingAddress: shippingAddressFromStorage,
+    },
+    userLogin: { userItems: userInfoFromStorage },
 };
 
 const middleware = [thunk];
