3fddab7878ca2a3c85964d5fc1b2949ab2761121
auxfuse <aux.fuse@yahoo.com>
2020-11-14 16:01:08 +0000

    Logout reset and redirect

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,6 +1,6 @@
 import {
     USER_DETAILS_FAIL,
-    USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS,
+    USER_DETAILS_REQUEST, USER_DETAILS_RESET, USER_DETAILS_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -9,6 +9,7 @@ import {
     USER_REGISTER_REQUEST,
     USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS
 } from "../constants/userConstants";
+import { ORDER_LIST_MY_RESET } from "../constants/orderConstants";
 import axios from 'axios';
 
 export const login = (email, password) => async (dispatch) => {
@@ -42,6 +43,9 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('useInfo')
     dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
+    dispatch({type: ORDER_LIST_MY_RESET})
+    document.location.href = '/login'
 };
 
 export const register = (name, email, password) => async (dispatch) => {
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -34,7 +34,7 @@ const ProfileScreen = ({ location, history }) => {
         if(!userInfo) {
             history.push('/login')
         } else {
-            if(!user.name || success) {
+            if(!user || !user.name || success) {
                 dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'))
                 dispatch(listMyOrders())
@@ -119,17 +119,21 @@ const ProfileScreen = ({ location, history }) => {
                             </tr>
                         </thead>
                         <tbody>
-                        {orders.map(order => (
+                        {orders.map((order) => (
                             <tr key={order._id}>
                                 <td>{order._id}</td>
                                 <td>{order.createdAt.substring(0, 10)}</td>
                                 <td>{order.totalPrice}</td>
-                                <td>{order.isPaid ? order.paidAt.substring(0, 10) : (
-                                    <i className='fas fa-times' style={{color: 'red'}}/>
-                                )}</td>
-                                <td>{order.isDelivered ? order.deliveredAt.substring(0, 10) : (
-                                    <i className='fas fa-times' style={{color: 'red'}}/>
-                                )}</td>
+                                <td>
+                                    {order.isPaid ? order.paidAt.substring(0, 10) : (
+                                        <i className='fas fa-times' style={{color: 'red'}}/>
+                                    )}
+                                </td>
+                                <td>
+                                    {order.isDelivered ? order.deliveredAt.substring(0, 10) : (
+                                        <i className='fas fa-times' style={{color: 'red'}}/>
+                                    )}
+                                </td>
                                 <td>
                                     <LinkContainer to={`/order/${order._id}`}>
                                         <Button className='btn-sm' variant='light'>Details</Button>
@@ -139,8 +143,7 @@ const ProfileScreen = ({ location, history }) => {
                         ))}
                         </tbody>
                     </Table>
-                )
-                }
+                )}
             </Col>
         </Row>
     )
