4a80711922a5e5de72e474e2b5bb643fbe1cbb48
auxfuse <aux.fuse@yahoo.com>
2020-11-16 21:41:35 +0000

    Admin user list functionality and screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from "./screens/ShippingScreen";
 import PaymentScreen from "./screens/PaymentScreen";
 import PlaceOrderScreen from "./screens/PlaceOrderScreen";
 import OrderScreen from "./screens/OrderScreen";
+import UserListScreen from "./screens/userListScreen";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
                 <Route path='/register' component={RegisterScreen} />
                 <Route path='/login' component={LoginScreen} />
                 <Route path='/profile' component={ProfileScreen} />
+                <Route path='/admin/userlist' component={UserListScreen}/>
                 <Route path='/product/:id' component={ProductScreen} />
                 <Route path='/shipping' component={ShippingScreen} />
                 <Route path='/payment' component={PaymentScreen} />
