eea77c1f299111e140bfb8c56789d2e15c49d21d
auxfuse <aux.fuse@yahoo.com>
2020-12-10 13:50:46 +0000

    Admin create product functionality added

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -4,7 +4,13 @@ import {
     PRODUCT_LIST_FAIL,
     PRODUCT_DETAILS_REQUEST,
     PRODUCT_DETAILS_SUCCESS,
-    PRODUCT_DETAILS_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL
+    PRODUCT_DETAILS_FAIL,
+    PRODUCT_DELETE_REQUEST,
+    PRODUCT_DELETE_SUCCESS,
+    PRODUCT_DELETE_FAIL,
+    PRODUCT_CREATE_RESET,
+    PRODUCT_CREATE_REQUEST,
+    PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_FAIL
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -41,6 +47,21 @@ export const productDeleteReducer = (state = { product: {} }, action) => {
             return { loading: false, success: true };
         case PRODUCT_DELETE_FAIL:
             return { loading: false, error: action.payload };
+        default:
+            return state;
+    }
+};
+
+export const productCreateReducer = (state = { product: {} }, action) => {
+    switch (action.type) {
+        case PRODUCT_CREATE_REQUEST:
+            return { loading: true };
+        case PRODUCT_CREATE_SUCCESS:
+            return { loading: false, success: true, product: action.payload };
+        case PRODUCT_CREATE_FAIL:
+            return { loading: false, error: action.payload };
+        case PRODUCT_CREATE_RESET:
+            return {}
         default:
             return state;
     }
