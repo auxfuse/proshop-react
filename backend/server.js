2c78e39a07a9152ab747ebaa972291d56c1e684b
auxfuse <aux.fuse@yahoo.com>
2020-12-10 23:55:52 +0000

    Front end image upload

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import {notFound, errorHandler} from "./middleware/errorMiddleware.js";
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import uploadRoutes from './routes/uploadRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js';
 
 dotenv.config();
 
--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('');
     const [description, setDescription] = useState('');
     const [countInStock, setCountInStock] = useState();
+    const [uploading, setUploading ] = useState(false);
 
     const dispatch = useDispatch();
 
@@ -45,6 +47,29 @@ const ProductEditScreen = ({ match, history }) => {
             }
         }
     }, [dispatch, history, product, productId, successUpdate])
+
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image', file);
+        setUploading(true);
+
+        try {
+            const config = {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            };
+
+            const { data } = await axios.post('/api/upload', formData, config);
+
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.log(error);
+            setUploading(false);
+        }
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -96,6 +121,13 @@ const ProductEditScreen = ({ match, history }) => {
                                 value={image}
                                 onChange={(e) => setImage(e.target.value)}
                             />
+                            <Form.File
+                                id='image-file'
+                                label='Choose File'
+                                custom
+                                onChange={uploadFileHandler}
+                            />
+                            {uploading && <Loader />}
                         </Form.Group>
                         <Form.Group controlId='brand'>
                             <Form.Label>Brand:</Form.Label>
