cc518a60bf22532dee4a99a5f464409ea884f02f
auxfuse <aux.fuse@yahoo.com>
2020-12-11 17:12:31 +0000

    Mark Order as Delivered functionality added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -12,7 +12,7 @@ import {
     userDeleteReducer,
     userUpdateReducer
 } from "./reducers/userReducers";
-import {orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderListReducer, orderPayReducer} from "./reducers/orderReducers";
+import {orderCreateReducer, orderDeliverReducer, orderDetailsReducer, orderListMyReducer, orderListReducer, orderPayReducer} from "./reducers/orderReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -31,6 +31,7 @@ const reducer = combineReducers({
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
+    orderDeliver: orderDeliverReducer,
     orderListMy: orderListMyReducer,
     orderList: orderListReducer,
 });
